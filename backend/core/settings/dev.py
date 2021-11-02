3f6cfdba438a598fdc17d616a084e0dae19ddfb1
Henrik <henrikhiltunen1@gmail.com>
2021-11-02 18:08:55 +0000

    update prod dockerfile to provide env var to frontend

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'development')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["backend", "localhost", "127.0.0.1", 'http://0.0.0.0']
+ALLOWED_HOSTS = ["backend", "localhost", "127.0.0.1", '0.0.0.0']
 
 DATABASES = {
     'default': {
