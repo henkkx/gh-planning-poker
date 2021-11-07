8962f10352d11cc2d426ca8bb2269966b87c916f
Henrik <henrikhiltunen1@gmail.com>
2021-11-07 02:07:13 +0000

    fix: remove avatar url

--- frontend/src/components/Navbar/UserProfile.tsx
+++ frontend/src/components/Navbar/UserProfile.tsx
@@ -12,7 +12,7 @@ export const UserProfile: React.FC<Props> = (props) => {
   const { name, email, avatarUrl } = props;
   return (
     <HStack spacing={3} order={{ base: 1, md: 2 }} flex="1">
-      <Avatar name={name} src={avatarUrl} size="sm" />
+      <Avatar name={name} size="sm" />
       <Flex direction="column" display={{ base: "flex", md: "none" }}>
         <Text fontWeight="bold" lineHeight="shorter">
           {name}
