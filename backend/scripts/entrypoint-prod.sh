f30429090283a99538e65f9647494496585d9225
Henrik <henrikhiltunen1@gmail.com>
2021-11-12 17:03:05 +0000

    chore: remove reduntant comment

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -2,7 +2,6 @@
 cd backend
 python3 manage.py migrate --no-input
 
-# gunicorn --chdir backend core.wsgi
 service pgbouncer start
 daphne core.asgi:application -b 0.0.0.0 -p $PORT
 service pgbouncer stop
